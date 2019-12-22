36aff822a4de1a573e8d5f068ec7dd4a7a1c9017
zfmai <waltcow@gmail.com>
2019-12-22 17:17:32 +0800

    update ci settings

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,10 +2,6 @@ import request from '@/utils/request';
 import { LoginForm, LoginResult } from '@/interfaces/ILogin';
 import IAccount from '@/interfaces/IAccount';
 
-export async function query(): Promise<any> {
-  return request('/api/users');
-}
-
 export async function queryNotices(): Promise<any> {
   return request('/api/notices');
 }
