db24edc78fca456abc2510245425f7691fb1016a
zfmai <waltcow@gmail.com>
2020-09-15 10:48:40 +0800

    update global typing

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -1,20 +1,6 @@
-type EnvironmentType = 'dev' | 'prod';
-
-declare const REACT_APP_ENV: EnvironmentType | boolean;
-
 interface EnvironmentConfig {
   OSS_BUCKET_NAME: string;
   OSS_REGION: string;
   BASE_API_URL: string;
   OSS_ASSET_URL: string;
 }
-
-interface WebConfig {
-  PUBLIC_PATH: string;
-}
-
-interface ProxyConfig {
-  '/api/': object;
-}
-
-type EnvironmentConfigMap = Record<EnvironmentType, EnvironmentConfig>;
